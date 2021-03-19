2343c8422fa4629c429829e21bc6a8e6c0de0fc4
Lucas Fischer <lucas.fischer@me.com>
2021-03-19 11:16:52 +0800

    Initial Commit

---
+++ Shared/Morning_PagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morning_PagesApp.swift
+//  Shared
+//
+//  Created by Lucas Fischer on 19.03.21.
+//
+
+import SwiftUI
+
+@main
+struct Morning_PagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
