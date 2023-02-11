741a49a55c256b0b26c7dd06c9d33619c75d00bf
Lucas Fischer <lucas.fischer@me.com>
2023-02-11 13:44:49 +0800

    macOS: scroll to selected theme onAppear

--- macOS/AppearanceSettingsView.swift
+++ macOS/AppearanceSettingsView.swift
@@ -32,31 +32,38 @@ struct AppearanceSettingsView: View {
             
             if #available(macOS 13.0, *) {
                 LabeledContent("Theme:") {
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(Theme.allCases, id: \.self) { theme in
-                                Button {
-                                    preferences.theme = theme
-                                } label: {
-                                    ThemePreview(
-                                        theme: theme,
-                                        font: preferences.font,
-                                        isActive: theme == preferences.theme
-                                    )
+                    ScrollViewReader { proxy in
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(Theme.allCases, id: \.self) { theme in
+                                    Button {
+                                        preferences.theme = theme
+                                    } label: {
+                                        ThemePreview(
+                                            theme: theme,
+                                            font: preferences.font,
+                                            isActive: theme == preferences.theme
+                                        )
+                                        
+                                    }
+                                    .buttonStyle(.plain)
+                                    .id(theme)
                                 }
-                                .buttonStyle(.plain)
                             }
+                            .padding(8)
+                            .padding(.bottom)
+                            
                         }
-                        .padding(8)
-                        .padding(.bottom)
-                        
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(
+                                    Color(NSColor.textBackgroundColor)
+                                )
+                        )
+                        .onAppear {
+                            proxy.scrollTo(preferences.theme, anchor: .center)
+                        }
                     }
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .foregroundColor(
-                                Color(NSColor.textBackgroundColor)
-                            )
-                    )
                 }
             }
             
