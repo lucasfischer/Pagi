2251d200f7470acd4b7803b93bba099fede68947
Lucas Fischer <lucas.fischer@me.com>
2025-05-23 09:31:28 +0800

    update test targets

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -2,12 +2,12 @@
 //  Tests_iOS.swift
 //  Tests iOS
 //
-//  Created by Lucas Fischer on 19.03.21.
+//  Created by Lucas Fischer on 23.05.25.
 //
 
 import XCTest
 
-class Tests_iOS: XCTestCase {
+final class Tests_iOS: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,21 +22,20 @@ class Tests_iOS: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    @MainActor
     func testLaunchPerformance() throws {
-        if #available(macOS 11.0, iOS 15.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
         }
     }
 }
---
+++ Tests macOS/Tests_macOSLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Tests_macOSLaunchTests.swift
+//  Tests macOS
+//
+//  Created by Lucas Fischer on 23.05.25.
+//
+
+import XCTest
+
+final class Tests_macOSLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
